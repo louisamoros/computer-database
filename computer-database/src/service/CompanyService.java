601dfd32e10b4811fb317076ca9d01be9057b0ec
louis <lamoros@excilys.com>
2016-02-08 14:52:57 +0100

    Entity services implemented.

--- computer-database/src/service/CompanyService.java
+++ computer-database/src/service/CompanyService.java
@@ -1,5 +1,21 @@
 package service;
 
+import java.util.List;
+
+import model.Company;
+import persistence.CompanyDaoImpl;
+import persistence.DaoFactory;
+
 public class CompanyService {
+
+	CompanyDaoImpl companyDao = null;
+
+	public CompanyService() {
+		companyDao = DaoFactory.getCompanyDao();
+	}
+
+	public List<Company> getCompanies() {
+		return companyDao.getCompanies();
+	}
 
 }
--- computer-database/src/service/ComputerService.java
+++ computer-database/src/service/ComputerService.java
@@ -1,5 +1,21 @@
 package service;
 
+import java.util.List;
+
+import model.Computer;
+import persistence.ComputerDaoImpl;
+import persistence.DaoFactory;
+
 public class ComputerService {
 
+	ComputerDaoImpl computerDao = null;
+	
+	public ComputerService() {
+		computerDao = DaoFactory.getComputerDao();
+	}
+	
+	public List<Computer> getComputers() {
+		return computerDao.getComputers();
+	}
+	
 }
