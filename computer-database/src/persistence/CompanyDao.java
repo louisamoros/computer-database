03b9ce1ddb00fd2624b52121965ac31c40a9a34c
louis <lamoros@excilys.com>
2016-02-08 10:32:38 +0100

    Create persistence packages for company and computer dao implementation.

---
+++ computer-database/src/persistence/CompanyDao.java
@@ -0,0 +1,14 @@
+package persistence;
+
+import java.util.List;
+
+import model.Company;
+
+public class CompanyDao {
+
+	public List<Company> getCompanies() {
+		//TODO
+		return null;
+	}
+	
+}
---
+++ computer-database/src/persistence/ComputerDao.java
@@ -0,0 +1,33 @@
+package persistence;
+
+import java.util.List;
+
+import model.Computer;
+
+public class ComputerDao {
+
+	public Computer getComputer(int id) {
+		//TODO
+		return null;
+	}
+	
+	public List<Computer> getComputers() {
+		//TODO
+		return null;
+	}
+	
+	public Computer createComputer() {
+		//TODO
+		return null;
+	}
+	
+	public Computer updateComputer(int id) {
+		//TODO
+		return null;
+	}
+	
+	public void deleteComputer(int id) {
+		//TODO
+	}
+	
+}
