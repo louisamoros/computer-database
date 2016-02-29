ba30a8cf09e7f6ba1e6e87f3f4444fb9111a8055
louisamoros <amoros.louis@gmail.com>
2016-02-29 17:13:36 +0100

    Shell start up changed to send credentials. Command need to be run in different terminal.

--- docker/start-up.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker-compose up -d
-
-echo -n "Docker hub password: "
-read -s password
-
-docker exec jenkins-cdb docker login --username="louisamoros" --email="amoros.louis@gmail.com" --password="$password"
