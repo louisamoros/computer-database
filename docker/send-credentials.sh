8965cfafc4323affe8f15784f2006f0376e4c3cc
louisamoros <amoros.louis@gmail.com>
2016-03-03 14:51:39 +0100

    Move docker compose to other gitrepository. Not relevant to keep it here.

--- docker/send-credentials.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo -n "Docker hub password: "
-read -s PASSWORD
-
-echo "Sending credentials to container..."
-docker exec jenkins-cdb docker login --username="louisamoros" --email="amoros.louis@gmail.com" --password="$PASSWORD"
-
