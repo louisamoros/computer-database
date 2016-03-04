19be549009663f25a545c5b5e9fe609b3c8a40a3
louisamoros <amoros.louis@gmail.com>
2016-03-04 10:57:20 +0100

    Put back test with spring integration.

--- src/test/java/com/louisamoros/cdb/utest/CompanyServiceTest.java
+++ src/test/java/com/louisamoros/cdb/utest/CompanyServiceTest.java
@@ -1,14 +1,18 @@
 package com.louisamoros.cdb.utest;
 
 import com.louisamoros.cdb.dao.CompanyDao;
+import com.louisamoros.cdb.dao.ComputerDao;
 import com.louisamoros.cdb.model.Company;
 import com.louisamoros.cdb.service.CompanyService;
 
 import org.junit.Assert;
-import org.junit.BeforeClass;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,28 +22,30 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * The Class CompanyServiceTest.
- */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("test-application-context.xml")
+@ContextConfiguration("classpath:test-application-context.xml")
 public class CompanyServiceTest {
 
   private static Logger LOGGER = LoggerFactory.getLogger(CompanyServiceTest.class);
 
-  @Autowired
-  private static CompanyDao mockCompanyDao;
+  @Mock
+  private CompanyDao mockCompanyDao;
 
+  @Mock
+  private ComputerDao mockComputerDao;
+
+  @InjectMocks
   @Autowired
-  private static CompanyService companyService;
+  private CompanyService companyService;
 
   /**
-   * Inits the.
+   * Set up.
    */
-  @BeforeClass
-  public static void init() {
-    LOGGER.info("Init mocking companyDao...");
-    companyService.setCompanyDao(mockCompanyDao);
+  @Before
+  public void setUp() {
+    LOGGER.info("Initialize mocks [mockCompanyDao][mockComputerDao]...");
+    // scans the class annotation, instantiates mocks and inject them into the bean.
+    MockitoAnnotations.initMocks(this);
   }
 
   /**
@@ -48,10 +54,21 @@ public class CompanyServiceTest {
    * @return the all companies test
    */
   @Test
-  public void getAllCompaniesTest() {
-    LOGGER.info("getAllCompaniesTest...");
-    Mockito.when(mockCompanyDao.getAll()).thenReturn(new ArrayList<Company>());
-    Assert.assertTrue(companyService.getAll() instanceof List<?>);
+  public void getAllCompaniesOk() {
+    LOGGER.info("getAllCompaniesOk...");
+    Mockito.when(mockCompanyDao.getAll()).thenReturn(getCompanies());
+    Assert.assertTrue(companyService.getAll().size() == 3);
+  }
+
+  private List<Company> getCompanies() {
+    List<Company> companies = new ArrayList<>();
+    Company company = new Company.Builder().name("company1").id(1).build();
+    companies.add(company);
+    company = new Company.Builder().name("company2").id(2).build();
+    companies.add(company);
+    company = new Company.Builder().name("company3").id(3).build();
+    companies.add(company);
+    return companies;
   }
 
 }
--- src/test/java/com/louisamoros/cdb/utest/ComputerServiceTest.java
+++ src/test/java/com/louisamoros/cdb/utest/ComputerServiceTest.java
@@ -1,105 +1,118 @@
-/*
- * package com.louisamoros.cdb.utest;
- * 
- * import com.louisamoros.cdb.dao.ComputerDao; import com.louisamoros.cdb.dao.impl.ComputerDaoImpl;
- * import com.louisamoros.cdb.exception.InvalidComputerNameException; import
- * com.louisamoros.cdb.exception.InvalidDateOrderException; import
- * com.louisamoros.cdb.model.Computer; import com.louisamoros.cdb.service.ComputerService; import
- * com.louisamoros.cdb.service.impl.ComputerServiceImpl;
- * 
- * import org.junit.Assert; import org.junit.BeforeClass; import org.junit.Test; import
- * org.junit.runner.RunWith; import org.powermock.api.mockito.PowerMockito; import
- * org.powermock.core.classloader.annotations.PrepareForTest; import
- * org.powermock.modules.junit4.PowerMockRunner; import org.slf4j.Logger; import
- * org.slf4j.LoggerFactory;
- * 
- * import java.time.LocalDate; import java.util.ArrayList; import java.util.List;
- * 
- *//**
-  * The Class ComputerServiceTest.
-  */
-/*
- * @RunWith(PowerMockRunner.class)
- * 
- * @PrepareForTest(ComputerDaoImpl.class) public class ComputerServiceTest {
- * 
- * private static Logger LOGGER = LoggerFactory.getLogger(ComputerServiceTest.class); private static
- * ComputerDao mockComputerDao; private static ComputerService computerService;
- * 
- *//**
-  * Inits the.
-  */
-/*
- * @BeforeClass public static void init() { LOGGER.info("Init mocking computerDao...");
- * mockComputerDao = PowerMockito.mock(ComputerDaoImpl.class); computerService =
- * ComputerServiceImpl.INSTANCE; computerService.setComputerDao(mockComputerDao); }
- * 
- *//**
-  * Gets the all computers test.
-  *
-  * @return the all computers test
-  */
-/*
- * @Test public void getAllComputersTest() { LOGGER.info("getAllComputersTest...");
- * PowerMockito.when(mockComputerDao.getAll()).thenReturn(getComputers());
- * Assert.assertTrue(computerService.getAll() instanceof List<?>);
- * Assert.assertEquals(computerService.getAll().size(), 2); }
- * 
- *//**
-  * Creates the computer test.
-  */
-/*
- * @Test public void createComputerTest() { LOGGER.info("createComputerTest..."); Computer
- * computer1 = new Computer.Builder("computer1").company(null).introduced(null)
- * .discontinued(null).build(); PowerMockito.when(mockComputerDao.create(computer1)).thenReturn(1);
- * int createdComputerId = computerService.create(computer1); Assert.assertTrue(createdComputerId ==
- * 1); }
- * 
- *//**
-  * Creates the computer invalid name test.
-  */
-/*
- * @Test(expected = InvalidComputerNameException.class) public void createComputerInvalidNameTest()
- * { LOGGER.info("createComputerInvalidNameTest..."); Computer computer1 = new
- * Computer.Builder(null).company(null).discontinued(null) .introduced(null).build();
- * computerService.create(computer1); }
- * 
- *//**
-  * Creates the computer invalid date order test.
-  */
-/*
- * @Test(expected = InvalidDateOrderException.class) public void
- * createComputerInvalidDateOrderTest() { LOGGER.info("createComputerInvalidDateOrderTest...");
- * Computer computer1 = new Computer.Builder("computer1").company(null)
- * .discontinued(LocalDate.of(2000, 9, 9)).introduced(LocalDate.now()).build();
- * computerService.create(computer1); }
- * 
- *//**
-  * Delete computer test.
-  */
-/*
- * @Test public void deleteComputerTest() { LOGGER.info("deleteComputerTest...");
- * ArrayList<Computer> computers = getComputers(); computers.remove(1); mockComputerDao.delete(1);
- * PowerMockito.when(mockComputerDao.getAll()).thenReturn(computers);
- * Assert.assertEquals(computerService.getAll().size(), 1); }
- * 
- *//**
-  * Update computer test.
-  *//*
-     * @Test public void updateComputerTest() { LOGGER.info("updateComputerTest...");
-     * ArrayList<Computer> computers = getComputers(); Computer computer1 = new
-     * Computer.Builder("updatedComputer").company(null).discontinued(null)
-     * .introduced(null).build(); computers.set(1, computer1);
-     * PowerMockito.when(mockComputerDao.update(computer1)).thenReturn(computer1.getId());
-     * PowerMockito.when(mockComputerDao.getAll()).thenReturn(computers);
-     * Assert.assertEquals(computerService.getAll().size(), 2);
-     * Assert.assertTrue(computer1.equals(computers.get(1))); }
-     * 
-     * private ArrayList<Computer> getComputers() { Computer computer1 = new
-     * Computer.Builder("computer1").company(null).discontinued(null) .introduced(null).build();
-     * Computer computer2 = new Computer.Builder("computer2").company(null).discontinued(null)
-     * .introduced(null).build(); ArrayList<Computer> computers = new ArrayList<Computer>();
-     * computers.add(computer1); computers.add(computer2); return computers; }
-     * 
-     * }
-     */
+package com.louisamoros.cdb.utest;
+
+import com.louisamoros.cdb.dao.ComputerDao;
+import com.louisamoros.cdb.exception.InvalidComputerNameException;
+import com.louisamoros.cdb.exception.InvalidDateOrderException;
+import com.louisamoros.cdb.model.Computer;
+import com.louisamoros.cdb.service.ComputerService;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:test-application-context.xml")
+public class ComputerServiceTest {
+
+  private static Logger LOGGER = LoggerFactory.getLogger(ComputerServiceTest.class);
+
+  @Mock
+  private ComputerDao mockComputerDao;
+
+  @InjectMocks
+  @Autowired
+  private ComputerService computerService;
+
+  /**
+   * Inits the.
+   */
+  @Before
+  public void init() {
+    LOGGER.info("Initialize mocks [mockComputerDao]...");
+    // scans the class annotation, instantiates mocks and inject them into the bean.
+    MockitoAnnotations.initMocks(this);
+  }
+
+  @Test
+  public void getAllComputersOk() {
+    LOGGER.info("getAllComputersOk...");
+    Mockito.when(mockComputerDao.getAll()).thenReturn(getComputers());
+    Assert.assertTrue(computerService.getAll() instanceof List<?>);
+    Assert.assertEquals(computerService.getAll().size(), 2);
+  }
+
+  @Test
+  public void createComputerOk() {
+    LOGGER.info("createComputerOk...");
+    Computer computer1 = new Computer.Builder("computer1").company(null).introduced(null)
+        .discontinued(null).build();
+    Mockito.when(mockComputerDao.create(computer1)).thenReturn(1);
+    int createdComputerId = computerService.create(computer1);
+    Assert.assertTrue(createdComputerId == 1);
+  }
+
+  @Test(expected = InvalidComputerNameException.class)
+  public void createComputerInvalidNameKo() {
+    LOGGER.info("createComputerInvalidNameKo...");
+    Computer computer1 = new Computer.Builder(null).company(null).discontinued(null)
+        .introduced(null).build();
+    computerService.create(computer1);
+  }
+
+  @Test(expected = InvalidDateOrderException.class)
+  public void createComputerInvalidDateOrderKo() {
+    LOGGER.info("createComputerInvalidDateOrderKo...");
+    Computer computer1 = new Computer.Builder("computer1").company(null)
+        .discontinued(LocalDate.of(2000, 9, 9)).introduced(LocalDate.now()).build();
+    computerService.create(computer1);
+  }
+
+  @Test
+  public void deleteComputerOk() {
+    LOGGER.info("deleteComputerOk...");
+    ArrayList<Computer> computers = getComputers();
+    computers.remove(1);
+    mockComputerDao.delete(1);
+    Mockito.when(mockComputerDao.getAll()).thenReturn(computers);
+    Assert.assertEquals(computerService.getAll().size(), 1);
+  }
+
+  @Test
+  public void updateComputerTestOk() {
+    LOGGER.info("updateComputerOk...");
+    ArrayList<Computer> computers = getComputers();
+    Computer computer1 = new Computer.Builder("updatedComputer").company(null).discontinued(null)
+        .introduced(null).build();
+    computers.set(1, computer1);
+    Mockito.when(mockComputerDao.update(computer1)).thenReturn(computer1.getId());
+    Mockito.when(mockComputerDao.getAll()).thenReturn(computers);
+    Assert.assertEquals(computerService.getAll().size(), 2);
+    Assert.assertTrue(computer1.equals(computers.get(1)));
+  }
+
+  private ArrayList<Computer> getComputers() {
+    Computer computer1 = new Computer.Builder("computer1").company(null).discontinued(null)
+        .introduced(null).build();
+    Computer computer2 = new Computer.Builder("computer2").company(null).discontinued(null)
+        .introduced(null).build();
+    ArrayList<Computer> computers = new ArrayList<Computer>();
+    computers.add(computer1);
+    computers.add(computer2);
+    return computers;
+  }
+
+}
