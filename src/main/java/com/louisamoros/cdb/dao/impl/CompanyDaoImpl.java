08082707a6244a743e7d2a9cb30d41c5a7221a5b
louisamoros <amoros.louis@gmail.com>
2016-03-04 18:11:34 +0100

    Update company dao impl to make it work with jdbc template. More work on test has to be done.

--- src/main/java/com/louisamoros/cdb/dao/impl/CompanyDaoImpl.java
+++ src/main/java/com/louisamoros/cdb/dao/impl/CompanyDaoImpl.java
@@ -1,18 +1,20 @@
 package com.louisamoros.cdb.dao.impl;
 
 import com.louisamoros.cdb.dao.CompanyDao;
+import com.louisamoros.cdb.dao.mapper.CompanyRowMapper;
 import com.louisamoros.cdb.dao.util.QueryGenerator;
 import com.louisamoros.cdb.model.Company;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-
-import javax.sql.DataSource;
 
 @Repository
 public class CompanyDaoImpl implements CompanyDao {
@@ -20,18 +22,35 @@ public class CompanyDaoImpl implements CompanyDao {
   public static Logger LOGGER = LoggerFactory.getLogger(CompanyDao.class);
 
   @Autowired
-  private DataSource dataSource;
+  private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
   @Override
   public List<Company> getAll() {
-    QueryGenerator qg = new QueryGenerator.Builder().select("*").from("company").build();
-    return new ArrayList<Company>();
-    // return companies;
+    // @formatter:off
+    QueryGenerator queryGenerator = new QueryGenerator
+        .Builder()
+        .select("*")
+        .from("company")
+        .build();
+    // @formatter:on
+    LOGGER.info(queryGenerator.getQuery().toString());
+    return namedParameterJdbcTemplate.query(queryGenerator.getQuery().toString(), new HashMap(),
+        new CompanyRowMapper());
   }
 
   @Override
   public void delete(int companyId) {
-    QueryGenerator qg = new QueryGenerator.Builder().deleteFrom("company").where("id=?").build();
+    // @formatter:off
+    QueryGenerator queryGenerator = new QueryGenerator
+        .Builder()
+        .deleteFrom("company")
+        .where("id=:companyId")
+        .build();
+    // @formatter:on
+    LOGGER.info(queryGenerator.getQuery().toString());
+    SqlParameterSource namedParameters = new MapSqlParameterSource("companyId", companyId);
+    namedParameterJdbcTemplate.queryForObject(queryGenerator.getQuery().toString(), namedParameters,
+        Integer.class);
   }
 
 }
